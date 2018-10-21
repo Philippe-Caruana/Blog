2ab2a4710b66fbdf573b967209cf1a2788e4a4b4
Philippe Caruana <philippecaruana@free.fr>
2018-10-21 18:42:30 +0200

    Add Manager Class : database connection

---
+++ model/Manager.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Projet8\Blog;
+
+class Manager
+{
+	protected function dbConnect()
+	{
+	    $db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+	    $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+	    
+	    return $db;
+	}
+}
