94ca2bd202110ff991b849bb9aebd5bc869ebc4b
Philippe Caruana <philippecaruana@free.fr>
2018-10-23 12:59:36 +0200

    Add attribute to database connection

--- model/Manager.php
+++ model/Manager.php
@@ -8,7 +8,8 @@ class Manager
 	{
 	    $db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
 	    $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-	    
+	    $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+
 	    return $db;
 	}
 }
