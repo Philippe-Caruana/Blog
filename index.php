b56c88a9da021ed0078c2e272cebe2192035030d
Philippe Caruana <philippecaruana@free.fr>
2018-10-24 22:17:34 +0200

    Update routing file : add addMember action in if statements

--- index.php
+++ index.php
@@ -20,6 +20,30 @@ try{
 		elseif ($_GET['action'] == "sign-up") {
 			displaySignUp();
 		}
+		elseif ($_GET['action'] == 'addMember') {
+			
+			if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['passwordConfirmation'])) {
+
+				if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+
+					if ($_POST['password'] == $_POST['passwordConfirmation']) {
+
+						addMember(strip_tags($_POST['username']), strip_tags($_POST['password']), strip_tags($_POST['email']));
+					}
+					else {
+						header("Location: /index.php?action=sign-up&error=different-passwords");
+					}
+				} 
+				else {
+
+					throw new \Exception("L'adresse email que vous avez renseigné ne dispose pas d'un format valide.");
+				}
+			} 
+			else {
+
+				throw new \Exception("Vous devez renseigner tous les champs.");
+			}
+		}
 		else {
 			header("Location: /");
 		}
